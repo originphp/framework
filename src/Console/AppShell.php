a15b48926a889fa03ac61e548a460619a718a2fc
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-01-02 12:36:13 +0100

    Added AppShell

---
+++ src/Console/AppShell.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console;
+
+use Origin\Console\Shell;
+
+class AppShell extends Shell
+{
+    /**
+     * Called when the Shell is constructed
+     *
+     * @param array $arguments from cli
+     * @return void
+     */
+    public function initialize(array $arguments)
+    {
+    }
+
+    /**
+     * Called before the shell method is called
+     *
+     * @return void
+     */
+    public function startup()
+    {
+    }
+    
+    /**
+     * Called after the shell method is called
+     *
+     * @return void
+     */
+    public function shutdown()
+    {
+    }
+}
