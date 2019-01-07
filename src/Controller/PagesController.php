91cd4142df812e411f03f1f2343334869daa96b2
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-01-07 21:22:03 +0100

    Added route for pages controller

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -1,7 +1,12 @@
 <?php
-
 namespace App\Controller;
 
+/**
+ * This is an optional controller for serving static page content. For example
+ * /pages/about_us will load the view file View/Pages/about_us.ctp
+ *
+ *  Router::add('/pages/*', ['controller'=>'Pages','action'=>'display']);
+ */
 use Origin\Controller\Controller;
 
 class PagesController extends Controller
@@ -11,12 +16,12 @@ class PagesController extends Controller
     public function display()
     {
         $args = func_get_args();
-
+    
         $count = count($args);
         if (!$count) {
             return $this->redirect('/');
         }
-
+   
         return $this->render('Pages/'.implode('/', $args));
     }
 }
