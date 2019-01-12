d689213eab29da78866913135a98085c5002b999
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-01-12 10:17:19 +0000

    Created Console Error Handler Test

---
+++ origin/tests/TestCase/Console/ErrorHandlerTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Test\Console;
+
+use Origin\Console\ErrorHandler;
+use Origin\Exception\Exception;
+
+class MockErrorHandler extends ErrorHandler
+{
+    public function setUp()
+    {
+        $this->consoleOutput = new MockConsoleOutput();
+    }
+    public function read()
+    {
+        return $this->consoleOutput->read();
+    }
+}
+class MockConsoleOutput
+{
+    protected $buffer = '';
+
+    public function read()
+    {
+        return $this->buffer;
+    }
+    public function write(string $data)
+    {
+        $this->buffer .= $data;
+    }
+}
+class ErrorHandlerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testRegisterError()
+    {
+        $ErrorHandler = new MockErrorHandler();
+        $ErrorHandler->register();
+        $this->expectException(\ErrorException::class);
+            
+        $original = unserialize('string');
+        restore_error_handler(); // Don't think
+    }
+    public function testRegisterException()
+    {
+        $ErrorHandler = new MockErrorHandler();
+        $ErrorHandler->register();
+        $ErrorHandler->setUp();
+        $this->expectException(Exception::class);
+        throw new Exception('Something went wrong');
+    }
+    public function testExceptionRender()
+    {
+        try {
+            throw new Exception('Something went wrong');
+        } catch (Exception $ex) {
+        }
+        $ErrorHandler = new MockErrorHandler();
+        $ErrorHandler->setUp();
+        $ErrorHandler->exception($ex);
+        $message = $ErrorHandler->read();
+        $expected = '<redBackground> Exception </redBackground> <yellow>Something went wrong</yellow>\n\n<cyan>origin/tests/TestCase/Console/ErrorHandlerTest.php</cyan> <yellowBackground> 66 </yellowBackground>\n\n<blue>61</blue> <white>        throw new Exception(\'Something went wrong\');</white>\n<blue>62</blue> <white>    }</white>\n<blue>63</blue> <white>    public function testExceptionRender()</white>\n<blue>64</blue> <white>    {</white>\n<blue>65</blue> <white>        try {</white>\n<blue>66</blue> <redBackground>            throw new Exception(\'Something went wrong\');</redBackground>\n<blue>67</blue> <white>        } catch (Exception $ex) {</white>\n<blue>68</blue> <white>        }</white>\n<blue>69</blue> <white>        $ErrorHandler = new MockErrorHandler();</white>\n<blue>70</blue> <white>        $ErrorHandler->setUp();</white>\n<blue>71</blue> <white>        $ErrorHandler->exception($ex);</white>\n\n<blueBackground> Stack Trace </blueBackground>\n\n<cyan>Exception </cyan><green></green>\n<white>origin/tests/TestCase/Console/ErrorHandlerTest.php</white> <yellowBackground> 66 </yellowBackground>\n\n<cyan>Origin\Test\Console\ErrorHandlerTest </cyan><green>testExceptionRender</green>\n<white>phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php</white> <yellowBackground> 1153 </yellowBackground>\n\n<cyan>PHPUnit\Framework\TestCase </cyan><green>runTest</green>\n<white>phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php</white> <yellowBackground> 842 </yellowBackground>\n\n<yellow>Use -backtrace to see the full backtrace.</yellow>\n\n';
+        $this->assertEquals($expected, str_replace("\n", '\n', $message));
+    }
+}
