c0db3c69c68ad9febc766c4428fbbd4494c99d40
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-01-16 10:26:28 +0000

    Adjusted comments

--- src/Model/AppModel.php
+++ src/Model/AppModel.php
@@ -13,22 +13,26 @@ class AppModel extends Model
     }
 
     /**
-     * Return either the query or true.
-     */
+    * Before find callback. Must return either the query or true to continue
+    * @return array|bool
+    */
     public function beforeFind($query = [])
     {
         return $query;
     }
 
+    /**
+     * After find callback.
+     * @return array|int $results
+     */
     public function afterFind($results)
     {
         return $results;
     }
 
     /**
-     * This must return true;.
-     *
-     * @return bool true
+     * Before validate callback
+     * @return bool must return true to continue
      */
     public function beforeValidate(Entity $entity)
     {
@@ -36,26 +40,52 @@ class AppModel extends Model
     }
 
     /**
-     * Called after validating data.
+     * After validate callback
+     * @param Entity $entity
+     * @return void
      */
     public function afterValidate(Entity $entity)
     {
+        parent::afterValidate($entity);
     }
 
+    /**
+     * Before save callback
+     *
+     * @param Entity $entity
+     * @param array $options
+     * @return bool must return true to continue
+     */
     public function beforeSave(Entity $entity, array $options = [])
     {
         return true;
     }
 
+    /**
+     * After save callback
+     *
+     * @param Entity $entity
+     * @param boolean $created
+     * @param array $options
+     * @return void
+     */
     public function afterSave(Entity $entity, bool $created, array $options = [])
     {
     }
 
+    /**
+     * Before delete callback
+     *
+     * @param boolean $cascade
+     * @return bool must return true to continue
+     */
     public function beforeDelete(bool $cascade = true)
     {
         return true;
     }
-
+    /**
+     * After delete callback
+     */
     public function afterDelete()
     {
     }
