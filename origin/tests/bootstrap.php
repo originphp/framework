e762dd8148acd935cca4dfc4cdd86cac815d1e0d
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-03-11 17:09:09 +0100

    Changed testing logs/tmp folder

--- origin/tests/bootstrap.php
+++ origin/tests/bootstrap.php
@@ -7,11 +7,11 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(dirname(__FILE__))));
 
 define('CONFIG', ROOT . DS . 'origin' . DS . 'tests' . DS . 'TestApp' . DS . 'config');
-define('LOGS', ROOT . DS . 'logs');
+define('LOGS', sys_get_temp_dir());
 define('ORIGIN', ROOT . DS . 'origin');
 define('SRC', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS . 'src');
 define('PLUGINS', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS . 'plugins');
-define('TMP', ROOT . DS . 'tmp');
+define('TMP', sys_get_temp_dir());
 define('WEBROOT', ROOT . DS . 'webroot');
 
 require ORIGIN . '/src/bootstrap.php';
