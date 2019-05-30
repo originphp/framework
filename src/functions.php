dd13326cdfc42611c82a0b829b79a78ead8bbff6
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-05-30 23:23:22 +0200

    Added some more comments

--- src/functions.php
+++ src/functions.php
@@ -162,8 +162,8 @@ function h($text)
 }
 
 /**
- * Gets or sets an Environment variable
- * 
+ * Gets or sets an Environment variable.
+ * @internal this should set ENV. SERVER is what the server sets. ENV is for you and me.
  * @param string $variable
  * @return string|null
  */
