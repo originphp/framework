7aeb90a092d4d4cddab48a6f8576fd5f7a4236cf
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-06-06 13:18:40 +0200

    Fixed typo

--- tests/TestApp/config/application.php
+++ tests/TestApp/config/application.php
@@ -11,7 +11,7 @@ Configure::write('debug', true); // goes in server
 /*
  * If you change namespace name then you will need to change:
  *  1. The namespace declaration in all your files in the src folder
- *  2. config/autoloader.php folder
+ *  2. config/bootstrap.php file
  */
 Configure::write('App.namespace', 'App');
 Configure::write('App.encoding', 'UTF-8');
