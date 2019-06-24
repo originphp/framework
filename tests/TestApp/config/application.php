b446b3c0e659ce84543d6da7f379b99e8a1bc49a
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-06-24 17:01:06 +0200

    Fixed test case configuration to work with Security.pepper

--- tests/TestApp/config/application.php
+++ tests/TestApp/config/application.php
@@ -23,7 +23,7 @@ Cache::config('default', ['engine' => 'File']);
  * Generate a random string such as md5(time()) and place
  * here. This is used with hashing and key generation by Security.
  */
-Configure::write('Security.salt', '-----ORIGIN PHP-----');
+Configure::write('Security.pepper', '-----ORIGIN PHP-----');
 
 /**
  * Recommended is true, if you don't want date,datetime,time or number fields to be parsed according
