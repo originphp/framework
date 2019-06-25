c608e5571d58a37809837a0c9708358eb501cbcc
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-06-25 09:04:59 +0200

    Fixed failure when running on different systems due to different amount of locales

--- tests/TestCase/Command/LocaleGeneratorCommandTest.php
+++ tests/TestCase/Command/LocaleGeneratorCommandTest.php
@@ -9,28 +9,29 @@ class LocaleGeneratorCommandTest extends OriginTestCase
     use ConsoleIntegrationTestTrait;
 
   
-    public function testRun(){
-
+    public function testRun()
+    {
         $backup = file_get_contents(CONFIG . DS . 'locales' . DS . 'en_GB.yml');
         $this->exec('locale:generate --force');
         $this->assertExitSuccess();
-        $this->assertOutputContains('Generated 722 locale definitions');
-             // Remove files
-             $files = array_diff(scandir(CONFIG . DS . 'locales'), ['.', '..']);
-             foreach($files as $file){
-                 unlink(CONFIG . DS . 'locales' . DS . $file);
-             }
-        file_put_contents(CONFIG . DS . 'locales' . DS . 'en_GB.yml',$backup);
+        $this->assertRegexp('/Generated [0-9]{3} locale definitions/', $this->output());
+      
+        // Remove files
+        $files = array_diff(scandir(CONFIG . DS . 'locales'), ['.', '..']);
+        foreach ($files as $file) {
+            unlink(CONFIG . DS . 'locales' . DS . $file);
+        }
+        file_put_contents(CONFIG . DS . 'locales' . DS . 'en_GB.yml', $backup);
     }
 
-    public function testQualityCheck(){
+    public function testQualityCheck()
+    {
         $this->exec('locale:generate en_GB --force');
         $this->assertExitSuccess();
         $this->assertOutputContains('Generated 1 locale definitions');
 
-       // $hash = md5(file_get_contents('/var/www/config/locales/en_GB.yml'));
-        $this->assertEquals('00c71cc38eec600727fd82e06e59a730',md5(file_get_contents(CONFIG . DS . 'locales' . DS . 'en_GB.yml')));
-       # Dont DELETE THIS. This is required by
+        // $hash = md5(file_get_contents('/var/www/config/locales/en_GB.yml'));
+        $this->assertEquals('00c71cc38eec600727fd82e06e59a730', md5(file_get_contents(CONFIG . DS . 'locales' . DS . 'en_GB.yml')));
+        # Dont DELETE THIS. This is required by
     }
-  
 }
