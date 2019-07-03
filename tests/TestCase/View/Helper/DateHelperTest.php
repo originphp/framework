c5acc23fdfa1137638fc8998e318e82a7d5759f9
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-07-03 14:18:10 +0200

    Added timeAgoInWordsTest

--- tests/TestCase/View/Helper/DateHelperTest.php
+++ tests/TestCase/View/Helper/DateHelperTest.php
@@ -32,4 +32,28 @@ class DateHelperTest extends \PHPUnit\Framework\TestCase
         $this->assertNull($this->Date->format(null));
         $this->assertEquals('01/03/2019', $this->Date->format('2019-03-01 16:26:00', 'd/m/Y'));
     }
+
+    public function testTimeAgoInWords()
+    {
+        // Future
+        $now = date('Y-m-d H:i:s', strtotime('+1 minute'));
+        $this->assertEquals('1 minute', $this->Date->timeAgoInWords($now));
+        // Past
+        $now = date('Y-m-d H:i:s', strtotime('-1 hour'));
+        $this->assertEquals('1 hour ago', $this->Date->timeAgoInWords($now));
+
+        // Plural
+        $now = date('Y-m-d H:i:s', strtotime('-2 day'));
+        $this->assertEquals('2 days ago', $this->Date->timeAgoInWords($now));
+
+        // Just now
+        $this->assertEquals('just now', $this->Date->timeAgoInWords(now()));
+
+        // Remaining
+        $now = date('Y-m-d H:i:s', strtotime('-2 month'));
+        $this->assertEquals('2 months ago', $this->Date->timeAgoInWords($now));
+
+        $now = date('Y-m-d H:i:s', strtotime('-1 year'));
+        $this->assertEquals('1 year ago', $this->Date->timeAgoInWords($now));
+    }
 }
