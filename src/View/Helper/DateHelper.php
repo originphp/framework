fbacbd322fe7492f3e4fc76928ecb613541b84df
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-07-03 12:57:38 +0200

    Added timeAgoInWords

--- src/View/Helper/DateHelper.php
+++ src/View/Helper/DateHelper.php
@@ -31,11 +31,62 @@ class DateHelper extends Helper
      * @param string $format date function compatiable string e.g 'H:i:s'
      * @return string
      */
-    public function format($dateString, string $format = null)
+    public function format(string $dateString = null, string $format = null)
     {
         if ($dateString) {
             return Date::format($dateString, $format);
         }
         return null;
+    }
+
+    /**
+     * Takes a datetime string and formats in a friendly way. e.g. 3 minutes ago
+     *
+     * @param string $datetime
+     * @return string
+     */
+    public function timeAgoInWords(string $datetime = null) : string
+    {
+        $time = strtotime($datetime);
+        if ($time === false) {
+            return null;
+        }
+
+        $now = time();
+        $startTime = $time;
+        $endTime = $now;
+
+        $isFuture = false;
+        if ($time > $now) {
+            $isFuture = true;
+            $startTime = $now;
+            $endTime = $time;
+        }
+
+        $difference = $endTime - $startTime;
+
+        if ($difference < 1) {
+            return 'just now';
+        }
+
+        $conditions = [
+            31104000 => 'year', //  12 * 30 * 24 * 60 * 60
+            2592000 => 'month', //  30 * 24 * 60 * 60
+            86400 => 'day', //  24 * 60 * 60
+            3600 => 'hour', //  60 * 60
+            60 => 'minute',
+            1 => 'second',
+        ];
+
+        foreach ($conditions as $seconds => $type) {
+            $result = $difference / $seconds;
+            if ($result  >= 1) {
+                $final = round($result);
+                if ($final > 1) {
+                    $type .= 's'; // inflect
+                }
+                return "{$final} {$type}" . ($isFuture === false ? ' ago' : '');
+            }
+        }
     }
 }
