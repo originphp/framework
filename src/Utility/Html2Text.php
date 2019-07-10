f1cc2130b122464eada69bf5ed0b19e772905e29
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-07-10 12:31:45 +0200

    Added deprecation warning

--- src/Utility/Html2Text.php
+++ src/Utility/Html2Text.php
@@ -29,6 +29,7 @@ class Html2Text
      */
     public static function convert(string $html) :string
     {
+        deprecationWarning('Html2Text is being deprecated Html::toText instead');
         /**
          * PHP BUG
          *  $html = '<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3>';
