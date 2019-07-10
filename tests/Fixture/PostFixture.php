9f05f9ad0b2c388e41b79b89ec7cc10a4b032e83
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-07-10 14:23:17 +0200

    Changed array style

--- tests/Fixture/PostFixture.php
+++ tests/Fixture/PostFixture.php
@@ -20,48 +20,48 @@ class PostFixture extends Fixture
 {
     public $datasource = 'test';
 
-    public $schema = array(
+    public $schema = [
       ' id' => ['type' => 'primaryKey'],
-         'title' => array(
+         'title' => [
            'type' => 'string',
            'limit' => 255,
            'null' => false,
-         ),
+         ],
          'body' => 'text',
-         'published' => array(
-           'type' => 'integer',
+         'published' => [
+           'type' => 'integer', // should be bool but needs testing
            'default' => '0',
            'null' => false,
            
-         ),
+         ],
          'created' => 'datetime',
          'modified' => 'datetime',
-     );
+     ];
 
-    public $records = array(
-         array(
+    public $records = [
+         [
            'id' => 1,
            'title' => 'First Post',
            'body' => 'Post body goes here',
            'published' => '1',
            'created' => '2018-12-19 13:29:10',
            'modified' => '2018-12-19 13:30:20',
-         ),
-         array(
+         ],
+         [
            'id' => 2,
            'title' => 'Second Post',
            'body' => 'Post body goes here',
            'published' => '1',
            'created' => '2018-12-19 13:31:30',
            'modified' => '2018-12-19 13:32:40',
-         ),
-         array(
+         ],
+         [
            'id' => 3,
            'title' => 'Third Post',
            'body' => 'Third Post Body',
            'published' => '1',
            'created' => '2018-12-19 13:33:50',
            'modified' => '2018-12-19 13:34:59',
-         ),
-     );
+         ],
+     ];
 }
