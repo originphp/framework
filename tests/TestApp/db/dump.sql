46cf9b406a871e128b4f268c40d3abb49bd36667
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-08-08 14:49:41 +0200

    removed

--- tests/TestApp/db/dump.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "posts" (
-  "id" SERIAL,
-  "title" VARCHAR(255) NOT NULL,
-  "body" TEXT,
-  "published" INTEGER DEFAULT 0,
-  "created" TIMESTAMP,
-  "modified" TIMESTAMP,
-  PRIMARY KEY (id)
-);
