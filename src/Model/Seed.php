690d4f2073d1fe380fa55732d13390e25864618b
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-08-09 19:41:53 +0200

    Added seed class

---
+++ src/Model/Seed.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Model;
+
+class Seed
+{
+    /**
+     * Creates the SQL statements for inserting
+     *
+     * @param Datasource $connection
+     * @return array
+     */
+    public function insertSql(Datasource $connection) : array
+    {
+        $out = [];
+        $properties = get_object_vars($this);
+        foreach (array_keys($properties) as $table) {
+            foreach ($this->{$table} as $record) {
+                $builder = $connection->queryBuilder($table);
+                $sql = $builder->insert($record)->write();
+                $out[] = [$sql,$builder->getValues()];
+            }
+        }
+
+        return $out;
+    }
+}
