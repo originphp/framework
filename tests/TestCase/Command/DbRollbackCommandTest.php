9204b517b6d3be7d8f7950b0f46eea72bb9e9ded
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-08-12 12:05:34 +0200

    Added Db::rollback command

---
+++ tests/TestCase/Command/DbRollbackCommandTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Origin\Test\Command;
+
+use Origin\Model\ConnectionManager;
+use Origin\TestSuite\OriginTestCase;
+use Origin\TestSuite\ConsoleIntegrationTestTrait;
+
+class DbRollbackCommandTest extends OriginTestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    public $fixtures = ['Origin.Migration'];
+
+    public function testRollback()
+    {
+        // Load the Migrations from file
+        $this->exec('db:migrate --datasource=test');
+        $this->assertExitSuccess();
+        $this->assertOutputContains('Migration Complete. 3 migrations in 0 ms');
+
+        $this->exec('db:rollback --datasource=test');
+        $this->assertExitSuccess();
+        $this->assertOutputContains('Rollback Complete. 1 migrations in 0 ms');
+
+        $this->exec('db:rollback --datasource=test');
+        $this->assertExitSuccess();
+        $this->assertOutputContains('Rollback Complete. 1 migrations in 0 ms');
+
+        $this->exec('db:rollback --datasource=test');
+        $this->assertExitSuccess();
+        $this->assertOutputContains('Rollback Complete. 1 migrations in 0 ms');
+    }
+
+    public function testNoMigrations()
+    {
+        $this->exec('db:rollback --datasource=test');
+        $this->assertExitSuccess();
+        $this->assertErrorContains('No migrations found'); // Its a warning
+    }
+
+    protected function tearDown() : void
+    {
+        parent::tearDown();
+        $ds = ConnectionManager::get('test');
+        $ds->execute('DROP table IF EXISTS foo');
+        $ds->execute('DROP table IF EXISTS bar');
+        $ds->execute('DROP table IF EXISTS foobar');
+    }
+}
