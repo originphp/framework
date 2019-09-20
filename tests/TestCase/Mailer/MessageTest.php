a72bb30c0a614fa5a64912f4d3b2d75d0e76350b
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-09-20 11:02:33 +0200

    Added message test

---
+++ tests/TestCase/Mailer/MessageTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Test\Mailer;
+
+use Origin\Mailer\Message;
+
+class MailerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testConstruct()
+    {
+        $message = new Message('header: value', 'foo');
+        $this->assertEquals('header: value', $message->header());
+        $this->assertEquals('foo', $message->body());
+        $this->assertEquals("header: value\r\n\r\nfoo", (string) $message);
+    }
+}
