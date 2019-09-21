30993d7939f8470f083e07380c4aaa3ffc968f67
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-09-21 17:34:51 +0200

    Fixed error

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -72,7 +72,7 @@ class Config
     {
         static::dot()->set($key, $value);
         if ($key === 'debug') {
-            ini_set('display_errors', '1');
+            ini_set('display_errors', (string) $value);
         }
     }
 
