7f8a3adc6f1f853a7214f19efadbd98d3f8b4bb5
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-09-22 10:31:44 +0200

    Security fixes

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -323,12 +323,15 @@ class Response
     {
         # Setup Options
         $options += ['name' => null,'download' => false,'type' => null];
-        if (! file_exists($filename)) {
-            throw new NotFoundException(sprintf('The requested file %s could not be found or read.', $filename));
-        }
+       
         if ($options['name'] === null) {
             $options['name'] = basename($filename);
         }
+
+        if (! file_exists($filename)) {
+            throw new NotFoundException(sprintf('The requested file %s could not be found or read.', $options['name']));
+        }
+
         if ($options['type'] === null) {
             $options['type'] = mime_content_type($filename);
         }
