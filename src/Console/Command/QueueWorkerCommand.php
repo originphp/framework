c4218049391391c6b3a6b331c337f872dda6ff67
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-01 11:13:07 +0200

    Switched to pcntl_async_signals

--- src/Console/Command/QueueWorkerCommand.php
+++ src/Console/Command/QueueWorkerCommand.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 /**
  * OriginPHP Framework
  * Copyright 2018 - 2019 Jamiel Sharief.
@@ -12,6 +12,7 @@ declare(strict_types = 1);
  * @link         https://www.originphp.com
  * @license      https://opensource.org/licenses/mit-license.php MIT License
  */
+
 namespace Origin\Console\Command;
 
 use Origin\Job\Job;
@@ -52,13 +53,13 @@ class QueueWorkerCommand extends Command
      *
      * @return void
      */
-    public function initialize() : void
+    public function initialize(): void
     {
         $this->addArgument('queue', [
             'description' => 'a queue name or a list of queues seperated by spaces',
             'type' => 'array',
         ]);
-      
+
         $this->configureOptions();
         $this->configureStyles();
     }
@@ -68,7 +69,7 @@ class QueueWorkerCommand extends Command
      *
      * @return void
      */
-    public function execute() : void
+    public function execute(): void
     {
         if ($this->supportsSignals()) {
             $this->setupSignalHandler();
@@ -110,7 +111,7 @@ class QueueWorkerCommand extends Command
      * @param integer $iteration
      * @return boolean
      */
-    protected function isRunning() : bool
+    protected function isRunning(): bool
     {
         return ($this->stopped === false);
     }
@@ -123,7 +124,7 @@ class QueueWorkerCommand extends Command
      * @param array $queues
      * @return void
      */
-    protected function daemon(array $queues) : void
+    protected function daemon(array $queues): void
     {
         $sleep = $this->options('sleep');
 
@@ -134,7 +135,7 @@ class QueueWorkerCommand extends Command
                     $ranJobs = true;
                 }
             }
-    
+
             if (! $ranJobs and $sleep) {
                 sleep($sleep);
             }
@@ -147,7 +148,7 @@ class QueueWorkerCommand extends Command
      * @param string $queue
      * @return bool
      */
-    protected function processQueue(string $queue) : bool
+    protected function processQueue(string $queue): bool
     {
         $job = $this->connection->fetch($queue);
 
@@ -168,17 +169,17 @@ class QueueWorkerCommand extends Command
      * @param \Origin\Queue\Job $job
      * @return void
      */
-    protected function dispatchJob(Job $job) : void
+    protected function dispatchJob(Job $job): void
     {
         $result = true;
-       
+
         if ($job->timeout and $this->supportsSignals()) {
             $this->setTimeout($job->timeout);
         }
 
         $this->writeOutput('<text>[{date}]</text> <cyan>{type}</cyan> <text>{name}</text> <green>{id}</green>', [
             'date' => date('Y-m-d G:i:s'),
-            'type' => $job->attempts() === 0?'Run':'Retry #' . $job->attempts(),
+            'type' => $job->attempts() === 0 ? 'Run' : 'Retry #' . $job->attempts(),
             'name' => $job->name,
             'id' => $job->id(),
         ], false);
@@ -190,7 +191,7 @@ class QueueWorkerCommand extends Command
 
         $this->writeOutput('<text> ({took}s)</text> {status}', [
             'took' => $end - $start,
-            'status' => $result ? '<pass> OK </pass>':'<fail> FAILED </fail>',
+            'status' => $result ? '<pass> OK </pass>' : '<fail> FAILED </fail>',
         ]);
 
         if ($job->timeout and $this->supportsSignals()) {
@@ -206,7 +207,7 @@ class QueueWorkerCommand extends Command
      * @param boolean $newLine
      * @return void
      */
-    public function writeOutput($message, array $context = [], bool $newLine = true) : void
+    public function writeOutput($message, array $context = [], bool $newLine = true): void
     {
         $message = $this->interpolate($message, $context);
         if ($newLine) {
@@ -216,11 +217,11 @@ class QueueWorkerCommand extends Command
     }
 
     /**
-       * Checks that PCNTL is supported
-       *
-       * @return boolean
-       */
-    protected function supportsSignals() : bool
+     * Checks that PCNTL is supported
+     *
+     * @return boolean
+     */
+    protected function supportsSignals(): bool
     {
         return extension_loaded('pcntl');
     }
@@ -231,7 +232,7 @@ class QueueWorkerCommand extends Command
      * @param integer $seconds
      * @return void
      */
-    protected function setTimeout(int $seconds) : void
+    protected function setTimeout(int $seconds): void
     {
         pcntl_alarm($seconds);
         pcntl_signal(SIGALRM, [$this, 'timedout']);
@@ -242,20 +243,20 @@ class QueueWorkerCommand extends Command
      *
      * @return void
      */
-    protected function unsetTimeout() : void
+    protected function unsetTimeout(): void
     {
         pcntl_alarm(0);
     }
 
     /**
-    * Timeout
-    *
-    * @return void
-    */
-    protected function timedout() : void
+     * Timeout
+     *
+     * @return void
+     */
+    protected function timedout(): void
     {
         $seconds = $this->options('timeout');
-       
+
         if (extension_loaded('posix')) {
             /**
              * e.g 2019-09-05 13:16:25] Run Job 6c95e38f-dfc6-415a-acbd-e9e4781e7e4f [ Killing ]
@@ -271,13 +272,14 @@ class QueueWorkerCommand extends Command
     /**
      * Setup signal capture
      *
+     * @internal sockets use blocking, so this wont help unless socket blocking disabled
      * @return void
      */
-    protected function setupSignalHandler() : void
+    protected function setupSignalHandler(): void
     {
-        //pcntl_async_signals(true);
-        declare(ticks = 1);
-       
+        pcntl_async_signals(true);
+        //declare(ticks=1);
+
         pcntl_signal(SIGTERM, [$this, 'cancelJob']);
         pcntl_signal(SIGINT, [$this, 'cancelJob']);
     }
@@ -287,7 +289,7 @@ class QueueWorkerCommand extends Command
      *
      * @return void
      */
-    protected function cancelJob() : void
+    protected function cancelJob(): void
     {
         $this->out('<green>> </green><white>Gracefully stopping... (press <yellow>Ctrl+C</yellow> again to force)</white>');
         $this->stopped = true;
@@ -347,11 +349,11 @@ class QueueWorkerCommand extends Command
     {
         # Setup
         $this->io->styles('pass', [
-            'background' => 'green','color' => 'white',
+            'background' => 'green', 'color' => 'white',
         ]);
 
         $this->io->styles('fail', [
-            'background' => 'lightRed','color' => 'white',
+            'background' => 'lightRed', 'color' => 'white',
         ]);
     }
 }
