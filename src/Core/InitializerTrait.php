4a008e8b3d15717297033c82596b14d6af5c285c
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-04 17:45:32 +0200

    Adjusted to use initialize prefix

--- src/Core/InitializerTrait.php
+++ src/Core/InitializerTrait.php
@@ -14,17 +14,20 @@ declare(strict_types = 1);
  */
 namespace Origin\Core;
 
+use ReflectionMethod;
+use ReflectionException;
+
 trait InitializerTrait
 {
-
     /**
-     * Initializes Traits where their is a method with the name of the Trait.
+     * Initializes Traits where their is a method initializeTraitNameWithoutTraitSuffix
      *
      * Example
      *
      *   trait DeletableTrait
      *   {
-     *      public function deleteable(){
+     *      protected function intializeDeleteable()
+     *      {
      *
      *      }
      *    }
@@ -44,19 +47,37 @@ trait InitializerTrait
         }
     
         $traits = array_unique($traits);
-      
+
+        $args = func_get_args() ?? [];
         foreach ($traits as $trait) {
             list($namespace, $className) = namespaceSplit($trait);
           
-            if (strpos($className, 'Trait') === false) {
-                continue;
+            $method = $className;
+            if (strpos($className, 'Trait') !== false) {
+                $method = substr($className, 0, -5);
             }
 
-            $method = lcfirst(substr($className, 0, -5));
-            
-            if (method_exists($this, $method)) {
-                $this->$method(...func_get_args());
+            $method =  'initialize' . $method;
+            if ($this->hasMethod($this, $method)) {
+                $this->$method(...$args);
             }
         }
+    }
+
+    /**
+     * Checks if the object has a method
+     *
+     * @param object $object
+     * @param string $method
+     * @return boolean
+     */
+    private function hasMethod(object $object, string $method) : bool
+    {
+        try {
+            $method = new ReflectionMethod($object, $method);
+            return ($method->isPublic() or $method->isProtected());
+        } catch (ReflectionException $e) {
+        }
+        return false;
     }
 }
