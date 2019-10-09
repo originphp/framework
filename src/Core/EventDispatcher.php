d4a0f05772d34df7d3e84a0602b2b5d279dd06e8
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-09 07:20:30 +0200

    Added EventDispatcher

---
+++ src/Core/EventDispatcher.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Origin\Core;
+
+trait EventDispatcher
+{
+    /**
+     * Dispatches an event on object
+     *
+     * @param string $method
+     * @param array $arguments
+     * @return mixed any be anything or nothing
+     */
+    protected function dispatchEvent(string $method,array $arguments=[])
+    {
+        if(method_exists($this,$method)){
+            return call_user_func_array([$this,$method],$arguments);
+        }
+    }
+}
