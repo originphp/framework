29042fbec2280eb78a57e11e2df2ec7c82c1b419
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-14 09:00:01 +0200

    Removed Cache config from TestApp

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * OriginPHP Framework
  * Copyright 2018 - 2019 Jamiel Sharief.
@@ -18,11 +19,11 @@ ini_set('display_errors', true);
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__DIR__));
 define('ORIGIN', dirname(__DIR__));
-define('CONFIG', ORIGIN . DS . 'tests'. DS . 'TestApp' . DS .'config');
+define('CONFIG', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS . 'config');
 define('LOGS', sys_get_temp_dir());
 
-define('SRC', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS .'app');
-define('TESTS', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS .'tests');
+define('SRC', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS . 'app');
+define('TESTS', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS . 'tests');
 define('APP', ORIGIN . DS . 'tests' . DS . 'TestApp');
 define('PLUGINS', ORIGIN . DS . 'tests' . DS . 'TestApp' . DS . 'plugins');
 define('TMP', sys_get_temp_dir());
@@ -33,14 +34,3 @@ if (! file_exists(TMP . DS . 'cache')) {
 }
 
 require ORIGIN . DS . 'src' . DS . 'bootstrap.php';
-
-/**
- * Disable Cache
- */
-use Origin\Cache\Cache;
-
-Cache::config('origin_model', [
-    'engine' => 'Null',
-    'prefix' => 'origin_model_',
-    'duration' => 0,
-]);
