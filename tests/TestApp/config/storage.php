f2fe2f6546dec44dffd7fe728c21699d52194693
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-15 10:34:19 +0200

    Removed storage config from test app

--- tests/TestApp/config/storage.php
+++ tests/TestApp/config/storage.php
@@ -1,11 +1,1 @@
 <?php
-/**
- * Storage Configuration
- * Engines are Local, Ftp, Sftp
- * @see https://www.originphp.com/docs/storage/
- */
-use Origin\Storage\Storage;
-
-Storage::config('default', [
-    'engine' => 'Local'
-]);
