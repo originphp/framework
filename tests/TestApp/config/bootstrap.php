4bad174cb55b84d167600dd127ea93f3a8ab7dc9
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-17 13:41:18 +0200

    Decoupled Elasticsearch

--- tests/TestApp/config/bootstrap.php
+++ tests/TestApp/config/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 use Origin\Core\Plugin;
 use Origin\Core\Autoloader;
-use Origin\Utility\Elasticsearch;
 
 require __DIR__ . '/paths.php';
 require ORIGIN . '/src/bootstrap.php';
@@ -25,21 +24,12 @@ include 'email.php';
 include 'queue.php';
 require 'routes.php';
 
-if (env('ELASTICSEARCH_HOST')) {
-    Elasticsearch::config('test', [
-        'host' => env('ELASTICSEARCH_HOST'),
-        'port' => 9200,
-        'ssl' => false,
-        'timeout' => 400,
-    ]);
-}
-
 /*
  * Load your plugins here
  * use Origin\Core\Plugin
  * @example Plugin::load('ContactManager');
  */
-Plugin::load('Make'); // This is for code gen you can remove
+Plugin::load('Make');
 
 /*
  * Initialize the plugins
