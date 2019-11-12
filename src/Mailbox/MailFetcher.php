80ef72f92ce85cb92e63eeb414eed9bbde98fb9e
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-11-12 14:58:09 +0100

    Added MailFetcher

---
+++ src/Mailbox/MailFetcher.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+declare(strict_types=1);
+namespace Origin\Mailbox;
+
+use Generator;
+use Origin\Core\Exception\Exception;
+use Origin\Core\Exception\InvalidArgumentException;
+
+class MailFetcher
+{
+    private $config = [];
+
+    /**
+     * @var Resource
+     */
+    private $connection;
+
+    /**
+     * Number of messages downloaded
+     *
+     * @var integer
+     */
+    private $counter = 0;
+
+    public function __construct(array $config)
+    {
+        $config += [
+            'host' => null,'port' => 143,'protocol' => 'imap','encryption' => null,'validateCert' => true,
+            'username' => null, 'password' => null,'timeout' => 30
+        ];
+        if ($config['encryption'] and ! in_array($config['encryption'], ['ssl','tls','notls'])) {
+            throw new InvalidArgumentException('Invalid encryption type');
+        }
+        if (! in_array($config['protocol'], ['pop3','imap'])) {
+            throw new InvalidArgumentException('Only pop3 and imap supported');
+        }
+        $this->config = $config;
+
+        # Set timeout
+        imap_timeout(IMAP_OPENTIMEOUT, $this->config['timeout']);
+        imap_timeout(IMAP_READTIMEOUT, $this->config['timeout']);
+        imap_timeout(IMAP_WRITETIMEOUT, $this->config['timeout']);
+        imap_timeout(IMAP_CLOSETIMEOUT, $this->config['timeout']);
+
+        # Connect
+        ini_set('default_socket_timeout', (string) $this->config['timeout']);
+    }
+
+    private function connect() : void
+    {
+        $errorNo = $errorMessage = null;
+        set_error_handler(function ($e, $m) use (&$errorNo,&$errorMessage) {
+            $errorNo = $e;
+            $errorMessage = $m;
+        });
+        try {
+            $this->connection = imap_open(
+                $this->connectionString(),
+                $this->config['username'],
+                $this->config['password']
+             );
+        } catch (\Exception $ex) {
+        }
+        restore_error_handler();
+
+        ini_restore('default_socket_timeout');
+   
+        imap_errors();
+        imap_alerts();
+         
+        if (! $this->connection) {
+            throw new Exception('Error connecting to ' . $this->config['host'] .':' . $this->config['port']);
+        }
+    }
+
+    /**
+     * Gets the count of the messages downloaded
+     *
+     * @return integer
+     */
+    public function count() : int
+    {
+        return $this->counter;
+    }
+
+    /**
+     * Download messages, this will return a Generator which use a foreach loop on even
+     * if it has no results. Generators do not implement countable.
+     *
+     * @param array $options Options support the following keys :
+     *  - limit:
+     * @return Generator|null
+     */
+    public function download(array $options = []) : ?Generator
+    {
+        $options += ['limit' => 0,'messageId' => null];
+
+        $this->connect();
+
+        # Download the messages
+        if ($this->config['protocol'] === 'imap') {
+            $out = $this->downloadImapMessages($options);
+        } else {
+            $out = $this->downloadPop3Messages($options);
+        }
+        
+        # Shutdown
+        imap_errors();
+        imap_alerts();
+      
+        imap_close($this->connection);
+
+        return $out ? $this->generator($out) : null;
+    }
+
+    /**
+     * Downloads messages using the Internet Message Access Protocol (IMAP), these messages
+     * are downloaded in reverse order first since messages are stored on server and starting from
+     * top means each time we have to check emails that could be existing from years ago. Once the messages
+     * are downloaded it then reverses the array so they are back in the order they were received.
+     * Using the messageID, you can sync to last message that you downloaded
+     *
+     * @param array $options
+     * @return array
+     */
+    private function downloadImapMessages(array $options) : array
+    {
+        $count = imap_num_msg($this->connection);
+
+        $out = [];
+        # Loop backwards for IMAP
+        for ($i = $count; $i >= 0;$i--) {
+            $header = imap_headerinfo($this->connection, $i);
+            if ($options['messageId'] === $header->message_id) {
+                break;
+            }
+            $out[] = $this->saveMessage($i);
+            $this->counter++;
+            if ($options['limit'] and $options['limit'] === $this->counter) {
+                break;
+            }
+        }
+       
+        return array_reverse($out);
+    }
+
+    /**
+     * Downloads messages using POP3
+     *
+     * @param array $options
+     * @return array
+     */
+    private function downloadPop3Messages(array $options) : array
+    {
+        $count = imap_num_msg($this->connection);
+
+        $out = [];
+        # Loop forward for POP
+        for ($i = 1; $i <= $count; $i++) {
+            $out[] = $this->saveMessage($i);
+            $this->counter++;
+            if ($options['limit'] and $options['limit'] === $this->counter) {
+                break;
+            }
+        }
+
+        return $out;
+    }
+
+    /**
+     * Disconnect
+     *
+     * @return void
+     */
+    private function disconnect() : void
+    {
+    }
+
+    /**
+     * Saves the message to a temp file
+     *
+     * @param integer $messageId
+     * @return string
+     */
+    private function saveMessage(int $messageId) : string
+    {
+        $tmpfile = tempnam(sys_get_temp_dir(), 'f');
+        $fh = fopen($tmpfile, 'w');
+        fwrite($fh, imap_fetchbody($this->connection, $messageId, ''));
+        fclose($fh);
+
+        return $tmpfile;
+    }
+
+    /**
+     * Creates the generator from the results
+     *
+     * @param array $out
+     * @return Generator
+     */
+    private function generator(array $out) : Generator
+    {
+        foreach ($out as $tmpfile) {
+            $message = file_get_contents($tmpfile);
+            unlink($tmpfile);
+            yield $message;
+        }
+    }
+
+    /**
+     * Creates the connection string
+     *
+     * @param array $config
+     * @return void
+     */
+    private function connectionString()
+    {
+        $args = [
+            $this->config['host'] . ':' . $this->config['port']
+        ];
+        
+        $args[] = $this->config['protocol'];
+
+        if ($this->config['encryption']) {
+            $args[] = $this->config['encryption'];
+        }
+        if ($this->config['validateCert'] === false) {
+            $args[] = 'novalidate-cert';
+        }
+        $mailbox = null;
+        if ($this->config['protocol'] === 'imap') {
+            $mailbox = 'INBOX';
+        }
+
+        return '{' . implode('/', $args) . '}' . $mailbox;
+    }
+}
---
+++ tests/TestCase/Mailbox/MailFetcherTest.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Test\Mailbox;
+
+use Generator;
+use Origin\Mailbox\MailFetcher;
+use Origin\Core\Exception\Exception;
+use Origin\Core\Exception\InvalidArgumentException;
+
+class MailFetcherTest extends \PHPUnit\Framework\TestCase
+{
+    public function testInvalidProtocol()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new MailFetcher(['host' => 'localhost','protocol' => 'sftp']);
+    }
+    public function testInvalidEncryption()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new MailFetcher(['host' => 'localhost','encryption' => 'pgp']);
+    }
+
+    public function testBadConnection()
+    {
+        $this->expectException(Exception::class);
+        $fetcher = new MailFetcher([
+            'host' => 'smtp.gmail.com','port' => 993,'encryption' => 'ssl','validateCert' => false,
+            'username' => 'me','password' => 'secret','timeout' => 1
+        ]);
+        $fetcher->download();
+    }
+
+    public function testDownloadImap()
+    {
+        if (! env('EMAIL_IMAP_USERNAME') or ! env('EMAIL_IMAP_PASSWORD')) {
+            $this->markTestSkipped(
+                'EMAIL username and password not setup'
+            );
+        }
+        $fetcher = new MailFetcher([
+            'host' => env('EMAIL_IMAP_HOST'),'port' => env('EMAIL_IMAP_PORT'),'encryption' => env('EMAIL_IMAP_ENCRYPTION', null),'validateCert' => false,
+            'username' => env('EMAIL_IMAP_USERNAME'),'password' => env('EMAIL_IMAP_PASSWORD')
+        ]);
+
+        $messages = $fetcher->download(['limit' => 1]);
+        $this->assertEquals(1, $fetcher->count());
+        // its a generator
+        foreach ($messages as $message) {
+            // do nothing
+        }
+        $this->assertIsString($message);
+        $this->assertNotEmpty($message);
+        $this->assertStringContainsString('From: ', $message);
+        $this->assertStringContainsString('To: ', $message);
+        $this->assertStringContainsString('Subject: ', $message);
+
+        $lines = explode("\n", str_replace("\r\n", "\n", $message));
+        $messageId = null;
+        foreach ($lines as $line) {
+            if (preg_match('/^Message-ID: (.*)/is', $line, $matches)) {
+                $messageId = $matches[1];
+            }
+        }
+        unset($fetcher); // Close connection
+
+        return $messageId;
+    }
+
+    /**
+     * @depends testDownloadImap
+     *
+     * @return void
+     */
+    public function testSync($messageId)
+    {
+        $fetcher = new MailFetcher([
+            'host' => env('EMAIL_IMAP_HOST'),'port' => env('EMAIL_IMAP_PORT'),'encryption' => env('EMAIL_IMAP_ENCRYPTION', null),'validateCert' => false,
+            'username' => env('EMAIL_IMAP_USERNAME'),'password' => env('EMAIL_IMAP_PASSWORD')
+        ]);
+
+        $fetcher->download(['limit' => 1,'messageId' => $messageId]);
+        $this->assertEquals(0, $fetcher->count());
+        unset($fetcher); // Close connection
+    }
+
+    /**
+     * Test connection, and if there are no emails skip rest of checks
+     *
+     * @return void
+     */
+    public function testDownloadPop3()
+    {
+        if (! env('EMAIL_POP3_USERNAME') or ! env('EMAIL_POP3_PASSWORD')) {
+            $this->markTestSkipped(
+                'EMAIL username and password not setup'
+            );
+        }
+        $fetcher = new MailFetcher([
+            'host' => env('EMAIL_POP3_HOST'),'port' => env('EMAIL_POP3_PORT'),'encryption' => env('EMAIL_POP3_ENCRYPTION', null),'validateCert' => false,
+            'username' => env('EMAIL_POP3_USERNAME'),'password' => env('EMAIL_POP3_PASSWORD'),'protocol' => 'pop3'
+        ]);
+        // there may or may not be messages if we keep running tests
+        $messages = $fetcher->download(['limit' => 1]);
+        
+        $this->assertInstanceOf(Generator::class, $messages);
+        if ($fetcher->count() === 0) {
+            unset($fetcher); // Close connection
+            $this->markTestSkipped('No Messages');
+        }
+
+        $this->assertEquals(1, $fetcher->count());
+        // its a generator
+        foreach ($messages as $message) {
+            // do nothing
+        }
+        $this->assertIsString($message);
+        $this->assertNotEmpty($message);
+        $this->assertStringContainsString('From: ', $message);
+        $this->assertStringContainsString('To: ', $message);
+        $this->assertStringContainsString('Subject: ', $message);
+
+        unset($fetcher); // Close connection
+    }
+}
