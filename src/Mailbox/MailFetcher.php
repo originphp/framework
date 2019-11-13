e45065169d96c9ad37eea09a73f995a5b27075db
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-11-13 19:53:02 +0100

    reworked

--- src/Mailbox/MailFetcher.php
+++ src/Mailbox/MailFetcher.php
@@ -32,7 +32,7 @@ class MailFetcher
      *
      * @var integer
      */
-    private $counter = 0;
+    private $count = 0;
 
     /**
      * Constructor
@@ -47,14 +47,14 @@ class MailFetcher
     public function __construct(array $config)
     {
         $config += [
-            'host' => null,'port' => 143,'protocol' => 'imap','encryption' => null,'validateCert' => true,
-            'username' => null, 'password' => null,'timeout' => 30
+            'host' => null, 'port' => 143, 'protocol' => 'imap', 'encryption' => null, 'validateCert' => true,
+            'username' => null, 'password' => null, 'timeout' => 30
         ];
-        
-        if (! in_array($config['protocol'], ['pop3','imap'])) {
+
+        if (!in_array($config['protocol'], ['pop3', 'imap'])) {
             throw new InvalidArgumentException('Only pop3 and imap supported');
         }
-        if ($config['encryption'] and ! in_array($config['encryption'], ['ssl','tls','notls'])) {
+        if ($config['encryption'] and !in_array($config['encryption'], ['ssl', 'tls', 'notls'])) {
             throw new InvalidArgumentException('Invalid encryption type');
         }
 
@@ -74,31 +74,114 @@ class MailFetcher
      *
      * @return void
      */
-    private function connect() : void
+    private function connect(): void
     {
+        /**
+         * Register error handler to catch errors
+         */
         $errorNo = $errorMessage = null;
-        set_error_handler(function ($e, $m) use (&$errorNo,&$errorMessage) {
+        set_error_handler(function ($e, $m) use (&$errorNo, &$errorMessage) {
             $errorNo = $e;
             $errorMessage = $m;
         });
-        try {
-            $this->connection = imap_open(
-                $this->connectionString(),
-                $this->config['username'],
-                $this->config['password']
-             );
-        } catch (\Exception $ex) {
-        }
-        restore_error_handler();
 
+        $this->connection = imap_open($this->connectionString(), $this->config['username'], $this->config['password']);
+
+        restore_error_handler();
         ini_restore('default_socket_timeout');
-   
-        imap_errors();
-        imap_alerts();
-         
-        if (! $this->connection) {
-            throw new Exception('Error connecting to ' . $this->config['host'] .':' . $this->config['port']);
+
+        if (!$this->connection or $errorMessage) {
+            throw new Exception('Error connecting to ' . $this->config['host'] . ':' . $this->config['port']);
         }
+    }
+
+    /**
+     * Download messages. The actual messages themselves are downloaded during the for loop iteration. If you are
+     * syncing an IMAP mailbox, there will be small delay first.
+     *
+     * @param array $options Options support the following keys :
+     *  - limit: max number of messages to download
+     *  - messageId: last message downloaded by IMAP. This will trigger mailbox SYNC
+     * @return \Generator
+     */
+    public function download(array $options = []): Generator
+    {
+        $options += ['limit' => 0, 'messageId' => null];
+
+        $this->connect();
+
+        $emails = imap_search($this->connection, 'ALL');
+
+        
+
+        # If IMAP last message
+        if ($options['messageId']) {
+            krsort($emails);
+            $emails = $this->mailboxSync($emails, $options['messageId']);
+        }
+
+        $emails = $emails ? $emails : [];
+        $this->count = count($emails);
+
+        if ($options['limit'] and count($emails) > $options['limit']) {
+            $emails = array_slice($emails, -$options['limit']);
+        }
+
+        return $this->generator($emails);
+    }
+
+    /**
+     * Syncs an IMAP mailbox, by looping through list of emails (in reverse order) then
+     * when it finds the message ID it stops.
+     *
+     * @param array $emails
+     * @param string $messageId
+     * @return array
+     */
+    private function mailboxSync(array $emails, string $messageId) : array
+    {
+        $out = [];
+        # Get messages to be fetched
+        foreach ($emails as $id) {
+            $header = imap_headerinfo($this->connection, $id);
+            if ($messageId === $header->message_id) {
+                break;
+            }
+            $out[] = $id;
+        }
+        return array_reverse($out);
+    }
+
+    /**
+     * Creates the generator which downloads emails
+     *
+     * @param array $emails
+     * @return \Generator
+     */
+    private function generator(array $emails): Generator
+    {
+        foreach ($emails as $msgnumber) {
+            yield imap_fetchbody($this->connection, $msgnumber, '');
+        }
+        print 'called';
+        $this->disconnect();
+    }
+
+    /**
+     * Disconnects a connection
+     *
+     * @return boolean
+     */
+    private function disconnect() : bool
+    {
+        if ($this->connection) {
+            imap_errors();
+            imap_alerts();
+            imap_close($this->connection);
+            $this->connection = null;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -106,125 +189,9 @@ class MailFetcher
      *
      * @return integer
      */
-    public function count() : int
+    public function count(): int
     {
-        return $this->counter;
-    }
-
-    /**
-     * Download messages, this will return a Generator which use a foreach loop on even
-     * if it has no results. Generators do not implement countable.
-     *
-     * @param array $options Options support the following keys :
-     *  - limit:
-     * @return \Generator|null
-     */
-    public function download(array $options = []) : ?Generator
-    {
-        $options += ['limit' => 0,'messageId' => null];
-
-        $this->connect();
-    
-        # Download the messages
-        if ($options['messageId']) {
-            $out = $this->downloadSync($options);
-        } else {
-            $out = $this->downloadStandard($options);
-        }
-        
-        # Shutdown
-        imap_errors();
-        imap_alerts();
-      
-        imap_close($this->connection);
-        $this->connection = null;
-
-        return $out ? $this->generator($out) : null;
-    }
-
-    /**
-     * Downloads messages in reverse order first since messages are stored on server and starting from
-     * top means each time we have to check emails that could be existing from years ago. Once the messages
-     * are downloaded it then reverses the array so they are back in the order they were received.
-     * Using the messageID, you can sync to last message that you downloaded
-     *
-     * @param array $options
-     * @return array
-     */
-    private function downloadSync(array $options) : array
-    {
-        $count = imap_num_msg($this->connection);
-
-        $out = [];
-        
-        # Loop backwards for IMAP
-        for ($i = $count; $i > 0;$i--) {
-            $header = imap_headerinfo($this->connection, $i);
-            if ($options['messageId'] === $header->message_id) {
-                break;
-            }
-            $out[] = $this->saveMessage($i);
-            $this->counter++;
-            if ($options['limit'] and $options['limit'] === $this->counter) {
-                break;
-            }
-        }
-       
-        return array_reverse($out);
-    }
-
-    /**
-     * Downloads messages (first download for IMAP or download for POP3)
-     *
-     * @param array $options
-     * @return array
-     */
-    private function downloadStandard(array $options) : array
-    {
-        $count = imap_num_msg($this->connection);
-
-        $out = [];
-        # Loop forward for POP
-        for ($i = 1; $i <= $count; $i++) {
-            $out[] = $this->saveMessage($i);
-            $this->counter++;
-            if ($options['limit'] and $options['limit'] === $this->counter) {
-                break;
-            }
-        }
-
-        return $out;
-    }
-
-    /**
-     * Saves the message to a temp file
-     *
-     * @param integer $messageId
-     * @return string
-     */
-    private function saveMessage(int $messageId) : string
-    {
-        $tmpfile = tempnam(sys_get_temp_dir(), 'f');
-        $fh = fopen($tmpfile, 'w');
-        fwrite($fh, imap_fetchbody($this->connection, $messageId, ''));
-        fclose($fh);
-
-        return $tmpfile;
-    }
-
-    /**
-     * Creates the generator from the results
-     *
-     * @param array $out
-     * @return \Generator
-     */
-    private function generator(array $out) : Generator
-    {
-        foreach ($out as $tmpfile) {
-            $message = file_get_contents($tmpfile);
-            unlink($tmpfile);
-            yield $message;
-        }
+        return $this->count;
     }
 
     /**
@@ -232,23 +199,30 @@ class MailFetcher
      *
      * @return string
      */
-    private function connectionString() : string
+    private function connectionString(): string
     {
         $args = [
             $this->config['host'] . ':' . $this->config['port'],
             $this->config['protocol']
         ];
-        
+
         if ($this->config['encryption']) {
             $args[] = $this->config['encryption'];
         }
-        
+
         if ($this->config['validateCert'] === false) {
             $args[] = 'novalidate-cert';
         }
-        
+
         $mailbox = $this->config['protocol'] === 'imap' ? 'INBOX' : null;
 
         return '{' . implode('/', $args) . '}' . $mailbox;
+    }
+
+    public function __destruct()
+    {
+        if ($this->connection) {
+            $this->disconnect();
+        }
     }
 }
