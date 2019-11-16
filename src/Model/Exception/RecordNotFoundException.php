4cda84be41af18a3f5c7d216801afb64d853c32a
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-11-16 17:01:12 +0100

    Added RecordNotFoundException

---
+++ src/Model/Exception/RecordNotFoundException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Model\Exception;
+
+use Origin\Model\Exception\NotFoundException;
+
+class RecordNotFoundException extends NotFoundException
+{
+    protected $defaultErrorCode = 404;
+}
