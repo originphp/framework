261b6eb950f9442d319034b309c975bb6da6348d
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-11-19 09:48:38 +0100

    Added test

---
+++ tests/TestCase/Mailbox/Job/MailboxCleanJobTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Test\Mailbox\Job;
+
+use Origin\Mailbox\Job\MailboxCleanJob;
+use Origin\Mailbox\Model\InboundEmail;
+use Origin\TestSuite\OriginTestCase;
+
+class MailboxCleanJobTest extends OriginTestCase
+{
+    public $fixtures = ['Origin.Mailbox'];
+
+    protected function setUp() : void
+    {
+        $this->loadModel('InboundEmail', [
+            'className' => InboundEmail::class
+        ]);
+    }
+
+    public function testJob()
+    {
+        $inboundEmail = $this->InboundEmail->first();
+
+        $result = (new MailboxCleanJob())->dispatchNow($inboundEmail);
+        
+        $this->assertTrue($result);
+        $this->assertEquals(0, $this->InboundEmail->count());
+    }
+}
