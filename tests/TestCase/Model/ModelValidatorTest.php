cb823edfe25e537fd2dc79d476f473ff6cb9ec2a
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-12-13 09:07:24 +0100

    reversed commit due to incorrect comment

--- tests/TestCase/Model/ModelValidatorTest.php
+++ tests/TestCase/Model/ModelValidatorTest.php
@@ -237,6 +237,102 @@ class ModelValidatorTest extends OriginTestCase
         $this->Validator->validates($data);
     }
 
+    public function testAlphaNumeric()
+    {
+        $this->assertTrue($this->Validator->alphaNumeric('abc123'));
+        $this->assertFalse($this->Validator->alphaNumeric('a-123'));
+    }
+
+    public function testBoolean()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->boolean(true));
+        $this->assertTrue($Validator->boolean(false));
+        $this->assertFalse($Validator->boolean(1));
+    }
+
+    public function testCustom()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->custom('abc', '/^[a-zA-Z ]+$/'));
+        $this->assertFalse($Validator->custom('abc1234', '/^[a-zA-Z ]+$/'));
+    }
+
+    public function testDate()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->date('2017-01-01'));
+        $this->assertFalse($Validator->date('2017-01-32'));
+
+        $this->assertTrue($Validator->date('01-01-2017', 'd-m-Y'));
+        $this->assertFalse($Validator->date('01-14-2017', 'd-m-Y'));
+
+        $this->assertTrue($Validator->date('01/01/2017', 'd/m/Y'));
+        $this->assertFalse($Validator->date('01/14/2017', 'd/m/Y'));
+    }
+
+    public function testDatetime()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->datetime('2017-01-01 17:32:00'));
+        $this->assertFalse($Validator->datetime('2017-01-01 28:32:00'));
+    }
+
+    public function testDecimal()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->decimal(256.0));
+        $this->assertTrue($Validator->decimal('512.00'));
+        $this->assertTrue($Validator->decimal(1024.256));
+        $this->assertTrue($Validator->decimal('2048.512'));
+        $this->assertFalse($Validator->decimal(32));
+        $this->assertFalse($Validator->decimal(64));
+    }
+
+    public function testEmail()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->email('john.smith123@example.com'));
+        $this->assertFalse($Validator->email('john.smith1234 @example.com'));
+       
+        $this->assertFalse($Validator->email('root@localhost'));
+        $this->assertFalse($Validator->email('john.smith[at]example.com'));
+        $this->assertFalse($Validator->email('john.smith@exam:ple.com'));
+    }
+
+    public function testEqualTo()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->equalTo(5, 5));
+        $this->assertTrue($Validator->equalTo(7, '7'));
+        $this->assertFalse($Validator->equalTo(10, 5));
+    }
+
+    public function testExtension()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->extension('bootstrap.css', 'css'));
+        $this->assertTrue($Validator->extension('bootstrap.css', ['js', 'css']));
+        $this->assertTrue($Validator->extension('Logo.JPG', ['gif', 'png', 'jpg']));
+        $this->assertFalse($Validator->extension('bootstrap.js', 'css'));
+
+        $post = ['name' => 'bootstrap.css'];
+        $this->assertTrue($Validator->extension($post, 'css'));
+    }
+
+    public function testInList()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->inList('new', ['draft', 'new', 'published']));
+        $this->assertFalse($Validator->inList('dropped', ['draft', 'new', 'published']));
+    }
+
+    public function testIp()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->ip('192.168.1.37'));
+    }
+
     public function testValidateCustomRule()
     {
         $validator = new MockValidator(new Widget());
@@ -245,16 +341,131 @@ class ModelValidatorTest extends OriginTestCase
         $this->assertFalse($validator->validate(2, 'isOne'));
     }
 
-    /**
-     * Test the new Validation class is being called
-     */
-    public function testValidationClassCalling()
+    public function testMaxLength()
     {
-        $this->Article->validate('title', 'alpha');
-        $article = $this->Article->new(['title' => 'foo']);
-        $this->assertTrue($this->Article->validates($article));
-        $article->title = 'foo123';
-        $this->assertFalse($this->Article->validates($article));
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->maxLength('string', 10));
+        $this->assertFalse($Validator->maxLength('string', 3));
+    }
+
+    public function testMinLength()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->minLength('password', 8));
+        $this->assertFalse($Validator->minLength('nada', 8));
+    }
+
+    public function testNotBlank()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->notBlank('foo'));
+        $this->assertFalse($Validator->notBlank(''));
+        $this->assertFalse($Validator->notBlank(null));
+        $this->assertTrue($Validator->notBlank(0));
+        $this->assertTrue($Validator->notBlank('0'));
+        $this->assertFalse($Validator->notBlank(' '));
+        $this->assertFalse($Validator->notBlank('   '));
+        $this->assertTrue($Validator->notBlank(' o'));
+        $this->assertTrue($Validator->notBlank('o '));
+    }
+
+    public function testNotEmpty()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->notEmpty('foo'));
+        $this->assertFalse($Validator->notEmpty(''));
+        $this->assertFalse($Validator->notEmpty(null));
+        $this->assertTrue($Validator->notEmpty(0));
+        $this->assertTrue($Validator->notEmpty('0'));
+    }
+
+    public function testNumeric()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->numeric(1));
+        $this->assertTrue($Validator->numeric('1'));
+        $this->assertFalse($Validator->numeric('one'));
+        $this->assertTrue($Validator->numeric(1.2));
+    }
+
+    public function testFloat()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->float(123.45));
+        $this->assertTrue($Validator->float('123.45'));
+        $this->assertFalse($Validator->float(123456));
+        $this->assertFalse($Validator->float('one'));
+        $this->assertFalse($Validator->float('12345'));
+    }
+
+    public function testInteger()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->integer(1));
+        $this->assertTrue($Validator->integer('1'));
+        $this->assertFalse($Validator->integer('one'));
+        $this->assertFalse($Validator->integer(1.2));
+        $this->assertFalse($Validator->integer('12-000'));
+        $this->assertFalse($Validator->integer('1234.56'));
+        $this->assertFalse($Validator->integer(10.00));
+        $this->assertFalse($Validator->integer('10.00'));
+        $this->assertFalse($Validator->integer(-10.00));
+        $this->assertFalse($Validator->integer('-10.00'));
+    }
+
+    public function testRange()
+    {
+        $Validator = $this->Validator;
+        $this->assertFalse($Validator->range('xxx', 5, 10));
+        $this->assertTrue($Validator->range(5, 5, 10));
+        $this->assertTrue($Validator->range(10, 5, 10));
+        $this->assertFalse($Validator->range(1, 5, 10));
+        $this->assertFalse($Validator->range(11, 5, 10));
+    }
+
+    public function testTime()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->time('10:15', 'H:i'));
+        $this->assertFalse($Validator->time('10.15', 'H:i'));
+    }
+
+    public function testUrl()
+    {
+        $Validator = $this->Validator;
+        $this->assertTrue($Validator->url('http://www.google.com', true));
+        $this->assertTrue($Validator->url('https://www.google.com', true));
+        $this->assertFalse($Validator->url('www.google.com', true));
+
+        $this->assertFalse($Validator->url('http://www.google.com', false));
+        $this->assertFalse($Validator->url('https://www.google.com', false));
+        $this->assertTrue($Validator->url('www.google.com', false));
+
+        $this->assertFalse($Validator->url('ftp://www.google.com', false));
+        $this->assertFalse($Validator->url('origin://www.google.com', false));
+    }
+
+    public function testMimeType()
+    {
+        $Validator = $this->Validator;
+        $post = ['tmp_name' => ORIGIN . DS .'phpunit.xml.dist'];
+        $this->assertTrue($Validator->mimeType($post, 'text/xml'));
+        $this->assertTrue($Validator->mimeType($post, ['text/xml']));
+        $this->assertFalse($Validator->mimeType($post, 'text/plain'));
+        $this->assertFalse($Validator->mimeType($post, ['text/plain']));
+
+        $this->assertTrue($Validator->mimeType(ORIGIN . DS .'phpunit.xml.dist', ['text/xml']));
+    }
+
+    public function testFileUpload()
+    {
+        $Validator = $this->Validator;
+        $post = ['tmp_name' => null,'error' => UPLOAD_ERR_NO_FILE];
+        $this->assertFalse($Validator->upload($post));
+        $this->assertTrue($Validator->upload($post, true));
+
+        $post = ['tmp_name' => null,'error' => UPLOAD_ERR_OK];
+        $this->assertTrue($Validator->upload($post));
     }
 
     /**
