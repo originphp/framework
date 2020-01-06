1f930a6ad360748660b2d9193d7a94e3f40a072a
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2020-01-06 13:37:49 +0100

    Improved Service Result object

--- src/Service/Result.php
+++ src/Service/Result.php
@@ -15,7 +15,24 @@ declare(strict_types = 1);
 namespace Origin\Service;
 
 /**
- * A result was either successful or
+ * The Result object is handling results from Service Objects. Typically you will use multiple services
+ * in an app, and those services an be shared accross apps. So you want to decide upon a style to use to
+ * prevent. The docs suggest a design based upon the Google JSON style guide, this means on success there
+ * is a data key and on an error there is an error key.
+ *
+ *  # Examples
+ *
+ *  $result = new Result(['success'=>true,'data'=>['foo'=>'bar']]);
+ *  $result = new Result(['success'=>false,'error'=>['message'=>'foo does not exist']]);
+ *
+ *  You don't have to set the success key, you can just use `data` OR `error `inline with the gle JSON style guide
+ *  the success and error methods will pick this up. For example:
+ *
+ *  $result = new Result([data'=>['foo'=>'bar']]);
+ *  $result = new Result(['error'=>['message'=>'foo does not exist']]);
+ *
+ *  if($result->success()) {}
+ *
  */
 class Result
 {
@@ -29,5 +46,81 @@ class Result
         foreach ($properties as $key => $value) {
             $this->$key = $value;
         }
+    }
+
+    /**
+     * Checks if the result was successful, this is done by checking if there is a data property or
+     * success property set to true
+     *
+     * @return boolean
+     */
+    public function success() : bool
+    {
+        return ((isset($this->success) and $this->success === true) or isset($this->data));
+    }
+
+    /**
+     * Checks if the result was an error, this is done by checking if there is a error property or
+     * success property set to false
+     *
+     * @return boolean
+     */
+    public function error() : bool
+    {
+        return ((isset($this->success) and $this->success === false) or isset($this->error));
+    }
+
+    /**
+    * Magic method is trigged when the object is treated as string,
+    *
+    * @return string
+    */
+    public function __toString()
+    {
+        return $this->toJson(['pretty' => true]);
+    }
+
+    /**
+     * Converts the result object to JSON
+     *
+     * @param array $options
+     * @return string
+     */
+    public function toJson(array $options = []) : string
+    {
+        $options += ['pretty' => false];
+
+        return json_encode($this->toArray(), $options['pretty'] ? JSON_PRETTY_PRINT : 0);
+    }
+
+    /**
+     * Converts the Result object into an Array
+     *
+     * @return array
+     */
+    public function toArray()  : array
+    {
+        return $this->convertToArray($this);
+    }
+
+    /**
+     * Recursively converts an object to an array
+     *
+     * @param object $object
+     * @return array
+     */
+    private function convertToArray(object $object) : array
+    {
+        if (method_exists($object, 'toArray') and ! $object instanceof Result) {
+            return $object->toArray();
+        }
+        $array = (array) $object;
+        foreach ($array as $property => $value) {
+            if (is_object($value)) {
+                $array[$property] = $this->convertToArray($value);
+            }
+        }
+
+        return $array;
     }
 }
--- tests/TestCase/Service/ResultTest.php
+++ tests/TestCase/Service/ResultTest.php
@@ -23,4 +23,45 @@ class ResultTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('value', $result->key);
         $this->assertEquals(['foo' => 'bar'], $result->array);
     }
+
+    public function testSuccess()
+    {
+        $result = new Result(['foo' => 'bar']);
+        $this->assertFalse($result->success());
+
+        $result = new Result(['success' => true]);
+        $this->assertTrue($result->success());
+
+        $result = new Result(['data' => []]);
+        $this->assertTrue($result->success());
+    }
+
+    public function testError()
+    {
+        $result = new Result(['foo' => 'bar']);
+        $this->assertFalse($result->error());
+
+        $result = new Result(['success' => false]);
+        $this->assertTrue($result->error());
+
+        $result = new Result(['error' => []]);
+        $this->assertTrue($result->error());
+    }
+
+    public function testToJson()
+    {
+        $result = new Result(['success' => true,'data' => ['foo' => 'bar']]);
+        $expected = '{"success":true,"data":{"foo":"bar"}}';
+        $this->assertEquals($expected, $result->toJson());
+        $this->assertEquals($expected, (string) $result);
+        $expected = <<< EOF
+{
+    "success": true,
+    "data": {
+        "foo": "bar"
+    }
+}
+EOF;
+        $this->assertEquals($expected, $result->toJson(['pretty' => true]));
+    }
 }
