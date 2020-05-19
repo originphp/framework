2d72981097037fe019a75ef70671a5ba394efecb
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2020-05-19 20:20:24 +0200

    Fixed git ignore exclusion

---
+++ tests/TestApp/config/app.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * app.php configuration file. For server specific vars use the .env file.
+ * The .env file will be parsed automatically and saved into .env.php. If you
+ * make changes to the .env file, then delete .env.php and a new one will be
+ * generated
+ * To access settings from here
+ *
+ * use Origin\Core\Config;
+ * $name = Config::read('App.name');
+ */
+return [
+    /**
+     * Debug level
+     * Set to true to display error messages (Development mode)
+     */
+    'debug' => env('APP_DEBUG', true),
+    'name' => 'Test Application',
+    'url' => env('APP_URL', 'http://localhost'),
+    'environment' => env('APP_ENV'),
+    'namespace' => 'App',
+    'encoding' => 'UTF-8',
+    'defaultTimezone' => 'UTC',
+    'sessionTimeout' => 3600,
+    'securityKey' => env('APP_KEY'),
+    'schemaFormat' => 'php',
+    'mailboxKeepEmails' => '+30 days'
+];
