d56969d9940d73b252ad9b6696d632072a7206c1
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2020-08-15 15:08:44 +0200

    Added test for nested translations

--- tests/TestApp/app/Locale/es.php
+++ tests/TestApp/app/Locale/es.php
@@ -3,4 +3,9 @@ return [
     'hello world' => 'Hola Mundo',
     'Hello {name}' => 'Hola {name}',
     'There is one apple|There are {count} apples' => 'Hay una manzana|Hay {count} manzanas',
+    'products' => [
+        'pro' => [
+            'name' => 'nube'
+        ]
+    ]
 ];
